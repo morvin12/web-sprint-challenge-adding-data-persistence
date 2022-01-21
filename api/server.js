f4110de28436d399fead9ed4455f39537515b8cd
Michael Orvin <morvin1212@gmail.com>
2022-01-20 17:23:55 -0700

    Server is running and package.json file is set up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h1>It Worked!!</h1>')
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+});
+
+module.exports = server;
